d7ae073049cbc930c6624ddc80b478e563ead7dc
Federico Lombardo <lombardo.federico@merimp.com>
2026-04-03 17:10:59 +0200

    Add a method to get an enum attribute from its string literal name

--- src/enums/enums.py
+++ src/enums/enums.py
@@ -18,7 +18,18 @@ class DirectionEnum(Enum):
     Direction name
     """
 
-    NORTH = 1
-    SOUTH = 2
-    EAST = 3
-    WEST = 4
+    NORTH = "NORTH"
+    SOUTH = "SOUTH"
+    EAST = "EAST"
+    WEST = "WEST"
+
+    @staticmethod
+    def from_str(raw_param: str):
+        """
+        Enable the enum to translate a raw string to an element of the Enum itself
+        e.g. DirectionEnum["NORTH"] -> DirectionEnum.NORTH
+
+        :param raw_param: Raw string to convert to an element of the Enum
+
+        """
+        return DirectionEnum[raw_param]
