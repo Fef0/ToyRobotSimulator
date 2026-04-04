1757b28f48602ea04620b67302cb8cace2824fc5
Federico Lombardo <lombardo.federico@merimp.com>
2026-04-04 12:27:20 +0200

    Forgot to add main (changed enums directory)

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from src.simulator.robot import Robot
 from src.simulator.map import Map
 from src.simulator.simulator import RobotSimulator
-from src.enums.enums import DirectionEnum
+from src.enums import DirectionEnum
 
 if __name__ == "__main__":
     robot = Robot("ToyRobot", 0, 0, DirectionEnum.NORTH)
     map = Map(5, 5)
 
-    robot_simulator = RobotSimulator(robot=robot, map=map, log_file="log.log")
+    robot_simulator = RobotSimulator(robot=robot, map=map, log_file="logs/log.log")
 
-    robot_simulator.run_simulation(sim_commands_file="input.txt", out_file="output.txt")
+    robot_simulator.run_simulation(sim_commands_file="data/input.txt", out_file="data/output.txt")
